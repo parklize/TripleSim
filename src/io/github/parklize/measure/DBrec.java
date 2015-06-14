ce16088567091835201d4fe5fd97a61ed21a7e3c
parklize <parklize@gmail.com>
2015-06-14 22:53:02 +0100

    Initialize project

---
+++ src/io/github/parklize/measure/DBrec.java
@@ -0,0 +1,5 @@
+package io.github.parklize.measure;
+
+public class DBrec {
+
+}
---
+++ src/io/github/parklize/worker/Worker.java
@@ -0,0 +1,10 @@
+package io.github.parklize.worker;
+
+public class Worker {
+
+	public static void main(String[] args) {
+		
+		System.out.println("DBrec Test");
+	}
+
+}
